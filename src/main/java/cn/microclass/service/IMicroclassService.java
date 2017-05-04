524fe07297238cc16720e98b414d23fdadbdcd6e
Admin <Admin>
2017-05-04 10:29:41 +0800

    提交了抛出异常

--- src/main/java/cn/microclass/service/IMicroclassService.java
+++ src/main/java/cn/microclass/service/IMicroclassService.java
@@ -20,38 +20,40 @@ public interface IMicroclassService {
 	/**
 	 * 查询消分业务相关信息  接口 exam003
 	 * @return
+	 * @throws Exception 
 	 */
-	List<BaseBean> xfStudyQuery(Study s);
+	List<BaseBean> xfStudyQuery(Study s) throws Exception;
 	
 	/**
 	 * 消分学习随机取题目
 	 * @param s
 	 * @return
+	 * @throws Exception 
 	 */
-	List<BaseBean> xfStudyAnswer(Study s);
+	List<BaseBean> xfStudyAnswer(Study s) throws Exception;
 	
 	/**
 	 * 消分学习答题
 	 * @param s
 	 * @return
 	 */
-	List<BaseBean> xfAnswerQuey(Study s);
+	List<BaseBean> xfAnswerQuey(Study s) throws Exception;
 	//********************************  以上为消分学习所有方法  ********************************************************
 			
 	/**
 	 * 6.34.3	行人、非机动车驾驶人道路交通安全学习查询接口 
 	 */
-	List<BaseBean> xrStudyQuery(Study s);
+	List<BaseBean> xrStudyQuery(Study s) throws Exception;
 	
 	/**
 	 * 行人、非机动车驾驶人道路交通安全学习随机取题接口 
 	 */
-	List<BaseBean> xrStudyAnswer(Study s);
+	List<BaseBean> xrStudyAnswer(Study s) throws Exception;
 	
 	/**
 	 * 行人、非机动车驾驶人道路交通安全学习答题
 	 */
-	List<BaseBean> xrAnswerQuey(Study s);
+	List<BaseBean> xrAnswerQuey(Study s) throws Exception;
 	
 	//********************************  行人、非机动车驾驶人道路交通安全学习所有方法  ********************************************************
 	
@@ -59,12 +61,12 @@ public interface IMicroclassService {
 	/**
 	 * 电动车违法随机取题
 	 */
-	List<BaseBean> ddcStudyAnswer(Study s);
+	List<BaseBean> ddcStudyAnswer(Study s)throws Exception;
 	
 	/**
 	 * 电动车违法答题方法
 	 */
-	List<BaseBean> ddcAnswerQuey(Study s);
+	List<BaseBean> ddcAnswerQuey(Study s)throws Exception;
 	//******************************** 以上为 电动车学习所有方法  ********************************************************
 	
 
